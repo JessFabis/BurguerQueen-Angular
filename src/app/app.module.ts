9deba2499025fe1d55a6ff232fbf9fdd253591ff
J.Fabiola Juárez Z <jefajuza@hotmail.com>
2019-05-13 01:30:08 -0500

    Dentro de la carpeta app se creo la carpeta components la cual contiene los componentes header, body y footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule
