4ab8c7442b21ea2d90c88ff4dd568145fc7c51dc
J.Fabiola Juárez Z <jefajuza@hotmail.com>
2019-05-13 23:23:12 -0500

    se agregaron mas componentes y se modifico el maquetado html y css del footer y header

---
+++ src/app/components/complementos/complementos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-complementos',
+  templateUrl: './complementos.component.html',
+  styleUrls: ['./complementos.component.css']
+})
+export class ComplementosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/hamburguesas/hamburguesas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hamburguesas',
+  templateUrl: './hamburguesas.component.html',
+  styleUrls: ['./hamburguesas.component.css']
+})
+export class HamburguesasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
